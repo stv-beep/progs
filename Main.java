e924cd1e65d9444a0e06e2baa039e34d14ecf83e
Aleix Algueró <59015816+stv-beep@users.noreply.github.com>
2020-06-30 20:10:10 +0200

    more functions
    added some pts system

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+package progs;
+
 import java.util.Scanner;
 public class Main {
 
@@ -9,20 +11,81 @@ public class Main {
     System.out.println("Escribe: \"1\" para piedra, \"2\" para papel o \"3\" para tijeras.	Y \"4\" para salir del programa: ");
   }
 
+  public static double piedra = 0.33;
+  public static double papel = 0.66;
+  public static double tijera = 0.99;
+  
   public static double cpuPPT(){
     double random = Math.random();
     return random;
   }
+  
+  public static double piedr(double rand){
+	  int pts = 0;
+	  if (rand <= piedra) {// -0.33
+		  pts = 1;
+		  System.out.println("La machine ha sacado piedra! EMPATE!\n");
+		  return pts;} 
+	  else if (rand > piedra && rand <= papel) {
+		  	pts = -1;
+		  	System.out.println("La machine ha sacado papel! HAS PERDIDO!\n");
+      return pts;
+    } else if (rand > piedra && rand <= tijera) {
+    		pts = 0;
+			System.out.println("La machine ha sacado tijera! HAS GANADO!\n");
+			return pts;
+			} else if (rand > tijera) {
+				pts = 10;
+      System.out.println("Esto es un easter egg, la verdad es que has tenido suerte...La máquina se ha vuelto loca y ha sacado algo fuera de las 3 opciones!\n");
+    return pts;}
+	  return pts;}
+  
+  public static double ppl(double rand){
+	  int pts = 0;
+	  if (rand <= piedra) {// -0.33
+		  pts = 1;
+		  System.out.println("La machine ha sacado piedra! HAS GANADO!\n");
+		  return pts;} 
+	  else if (rand > piedra && rand <= papel) {
+		  	pts = 0;
+		  	System.out.println("La machine ha sacado papel! EMPATE!\n");
+      return pts;
+    } else if (rand > piedra && rand <= tijera) {
+    		pts = -1;
+    		System.out.println("La machine ha sacado tijera! HAS PERDIDO! \n");
+			return pts;
+			} else if (rand > tijera) {
+				pts = 10;
+      System.out.println("Esto es un easter egg, la verdad es que has tenido suerte...La máquina se ha vuelto loca y ha sacado algo fuera de las 3 opciones!\n");
+    return pts;}
+	  return pts;}
+  
+  
+  public static double tij(double rand){
+	  int pts = 0;
+	  if (rand <= piedra) {//-0.33
+		  pts = -1;
+		  System.out.println("La machine ha sacado piedra! HAS PERDIDO!\n");
+		  return pts;} 
+	  else if (rand > piedra && rand <= papel) {
+		  	pts = 1;
+		  	System.out.println("La machine ha sacado papel! HAS GANADO!\n");
+      return pts;
+    } else if (rand > piedra && rand <= tijera) {
+    		pts = 0;
+    		System.out.println("La machine ha sacado tijera! EMPATE!\n");
+			return pts;
+			} else if (rand > tijera) {
+				pts = 10;
+      System.out.println("Esto es un easter egg, la verdad es que has tenido suerte...La máquina se ha vuelto loca y ha sacado algo fuera de las 3 opciones!\n");
+    return pts;}
+	  return pts;}
 
 
   public static void main(String[] args) {
     System.out.println("Bienvenido a un 1 pa 1 sin camisa contra la aleatoriedad de la machine a un PIEDRA, PAPEL, TIJERA. Suerte!\n");
     Scanner sc = new Scanner(System.in);
-    double piedra = 0.33;
-		double papel = 0.66;
-		double tijera = 0.99;
-
-
+    
     while (Main.running) {
       Menu();
 
@@ -31,42 +94,17 @@ public class Main {
     switch(opt){
       /*rock*/
       case 1:
-      if (rndm <= piedra) {
-        System.out.println("La machine ha sacado piedra! EMPATE!\n");
-      } else if (rndm > piedra && rndm <= papel) {
-        System.out.println("La machine ha sacado papel! HAS PERDIDO!\n");
-      } else if (rndm > piedra && rndm <= tijera) {
-				System.out.println("La machine ha sacado tijera! HAS GANADO!\n");
-			} else if (rndm > tijera) {
-        System.out.println("Esto es un easter egg, la verdad es que has tenido suerte...La máquina se ha vuelto loca y ha sacado algo fuera de las 3 opciones!\n");
-      }
+      piedr(rndm);
       break;
 
       /*paper*/
       case 2:
-      if (rndm <= papel) {
-        System.out.println("La machine ha sacado piedra! HAS GANADO!\n");
-      } else if (rndm > piedra && rndm <= papel) {
-        System.out.println("La machine ha sacado papel! EMPATE!\n");
-      } else if (rndm > piedra && rndm <= tijera) {
-				System.out.println("La machine ha sacado tijera! HAS PERDIDO! \n");
-			} else if (rndm > tijera) {
-        System.out.println("Esto es un easter egg, la verdad es que has tenido suerte...La máquina se ha vuelto loca y ha sacado algo fuera de las 3 opciones!\n");
-      }
-
+     ppl(rndm);
       break;
 
       /*scissors*/
       case 3:
-      if (rndm <= piedra) {
-        System.out.println("La machine ha sacado piedra! HAS PERDIDO!\n");
-      } else if (rndm > piedra && rndm <= papel) {
-        System.out.println("La machine ha sacado papel! HAS GANADO!\n");
-      } else if (rndm > piedra && rndm <= tijera) {
-        System.out.println("La machine ha sacado tijera! EMPATE!\n");
-      } else if (rndm > tijera) {
-        System.out.println("Esto es un easter egg, la verdad es que has tenido suerte...La máquina se ha vuelto loca y ha sacado algo fuera de las 3 opciones!\n");
-      }
+     tij(rndm);
       break;
 
       default:
@@ -74,10 +112,6 @@ public class Main {
       System.out.println("ciao");
 
     }
-
-
-
-
 
 
     }
